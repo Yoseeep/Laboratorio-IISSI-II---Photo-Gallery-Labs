957e6208969e86a885a9770b44924dde54c0b7a8
José Manuel GV <jmgv.ciencia@gmail.com>
2026-03-19 12:25:15 +0100

    Fix import path and update photo URL in photo detail renderer

--- web/js/renderers/photo_detail.js
+++ web/js/renderers/photo_detail.js
@@ -1,15 +1,15 @@
 "use strict";
 
-import { photoRenderer } from "/js/renderers/photos.js";
+import { photoRenderer } from "/web/js/renderers/photos.js";
 
 function main() {
-    let photoContainer = document.querySelector("#photo-details-column");
+    let photoContainer = document.getElementById("photo-details-column");
 
     let photo = {
         title: "Samoyed",
         description: "A very good boy.",
         userId: 1,
-        url: "https://i.ibb.co/tY1Jcnc/wlZCfCv.jpg",
+        url: "https://static.vecteezy.com/system/resources/previews/023/193/490/non_2x/illustration-of-cute-little-baby-penguin-isolated-on-white-animal-clipart-in-flat-style-vector.jpg",
         date: "12/01/1996",
     };
 
